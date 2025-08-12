db11487ce1e531ad03a62fa5b2f2cb3c151dd3f4
Scott McQuaig <scottmcquaig@gmail.com>
2025-08-12 23:01:06 +0000

    can we start connecting this to firestore yac-fantasy?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,14 @@
 
 import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-// import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 // import { getAuth } from 'firebase/auth';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCNNCVA79p4gMT-AZ2s3KpV4_Cb4IImE3A",
-  authDomain: "yac-fantasy-league.firebaseapp.com",
-  projectId: "yac-fantasy-league",
-  storageBucket: "yac-fantasy-league.appspot.com",
+  authDomain: "yac-fantasy.firebaseapp.com",
+  projectId: "yac-fantasy",
+  storageBucket: "yac-fantasy.appspot.com",
   messagingSenderId: "495683435548",
   appId: "1:495683435548:web:8786d0e791c7d9051c33d9",
 };
@@ -17,8 +17,7 @@ const firebaseConfig: FirebaseOptions = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const storage = getStorage(app);
-// const db = getFirestore(app);
+const db = getFirestore(app);
 // const auth = getAuth(app);
 
-// export { app, db, auth };
-export { app, storage };
+export { app, db, storage };
