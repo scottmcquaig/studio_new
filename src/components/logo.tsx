38603f29420b71f43013923571c01168819a55d0
Scott McQuaig <scottmcquaig@gmail.com>
2025-08-15 17:59:48 +0000

    Please replace the icon next to the site title ("Big Brother 27") with t

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,21 +4,18 @@ export const Logo = ({ className }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill="currentColor"
     className={cn("text-primary", className)}
     aria-label="YAC Fantasy League Logo"
   >
-    <path d="M20 7H4c-1.1 0-2 .9-2 2v9c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2z" />
-    <path d="M12 11v6" />
-    <path d="M16 11v6" />
-    <path d="M8 11v6" />
-    <path d="M12 2v5" />
-    <path d="M9 2h6" />
-    <path d="M17 2l2 2" />
-    <path d="M7 2L5 4" />
+    <path
+      d="M19.71,6.29,18,4.58V4a1,1,0,0,0-2,0V8.42L15.71,6.29a1,1,0,0,0-1.42,1.42L16.17,9.6A1,1,0,0,0,17,10a1,1,0,0,0,.71-.29l1.88-1.88A1,1,0,1,0,19.71,6.29Z"
+    />
+    <path
+      d="M8.29,6.29,6,8.42V4A1,1,0,0,0,4,4V8.42L2.29,6.29A1,1,0,1,0,.88,7.71L2.76,9.59A1,1,0,0,0,3.47,10a1,1,0,0,0,.71-.29L6.88,7.71A1,1,0,1,0,8.29,6.29Z"
+    />
+    <path
+      d="M19,11H5a1,1,0,0,0-1,1v9a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1V12A1,1,0,0,0,19,11ZM8,20H6V14H8Zm5,0H11V14h2Zm5,0H16V14h2Z"
+    />
   </svg>
 );
