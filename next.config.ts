05cb4dba3ff8c42fcc57f3e1ea6dae4471c82eda
Scott McQuaig <scottmcquaig@gmail.com>
2025-09-09 18:32:54 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -25,11 +25,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-    ],
-  }
 };
 
 export default nextConfig;
